5d17caa06f0a2a46abb065529e1730053c86c617
CommanderHux <142996278+CommanderHux@users.noreply.github.com>
2025-06-29 13:20:12 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 function setup() {
-  noCanvas();  // canvas not needed for this test
-  sendToSheet("TestUser", 123);
+  noCanvas();  // Just testing, so no need for a canvas
+  sendToSheet("Huxley", 123);
 }
 
 function sendToSheet(name, score) {
-  fetch('https://script.google.com/macros/s/AKfycbzolULzyozcc4qkQXWymLVZAYQwKWWFzwKD81Y1JB1FPyXa4qoOEk-qNexUx5veBOHy/exec', {
+  const formData = new URLSearchParams();
+  formData.append("name", name);
+  formData.append("score", score);
+
+  fetch('https://script.google.com/macros/s/AKfycbywE-tgvR1IDdHA1LOAWpZZ8DRDnQ8kkY7S1XZ8V3h8CCF42F_CP18FFaciKd2APQ5U/exec', {
     method: 'POST',
-    body: JSON.stringify({ name: name, score: score }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
+    body: formData
   })
-  .then(res => res.text())
-  .then(response => console.log("✅ Response from script:", response))
+  .then(() => console.log("✅ Data sent"))
   .catch(err => console.error("❌ Fetch error:", err));
 }
