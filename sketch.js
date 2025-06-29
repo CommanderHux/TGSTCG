c3117139cc533846490d7c1be59c852d919adaf0
CommanderHux <142996278+CommanderHux@users.noreply.github.com>
2025-06-29 13:33:37 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
 function setup() {
   noCanvas();  // Just testing, so no need for a canvas
-  sendToSheet("Huxley", 123);
+  sendToSheet();
 }
 
 function sendToSheet(name, score) {
-  fetch('https://script.google.com/macros/s/AKfycbwC4E-5MPpiUhJkr3NRhAK1A5S13umAgBpjwRK4nRo/dev', {
-  method: 'POST',
-  mode: 'no-cors',  // 👈 key addition
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ name, score })
-});
+  fetch("https://script.google.com/macros/s/AKfycbwC4E-5MPpiUhJkr3NRhAK1A5S13umAgBpjwRK4nRo/dev", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ message: "Hello from the browser!" })
+})
+.then(res => res.text())
+.then(response => console.log(response));
 }
